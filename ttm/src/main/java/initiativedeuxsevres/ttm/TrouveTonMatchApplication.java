325df2e2d8cd37586d683310d27fa4953c6a422c
CHARRIER Charlotte <charlotte.charrier97@gmail.com>
2024-09-20 09:26:36 +0200

    feat: Initalisation spring boot

---
+++ ttm/src/main/java/initiativedeuxsevres/ttm/TrouveTonMatchApplication.java
@@ -0,0 +1,13 @@
+package initiativedeuxsevres.ttm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrouveTonMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrouveTonMatchApplication.class, args);
+	}
+
+}
