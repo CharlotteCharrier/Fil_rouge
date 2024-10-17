a1e8aad3b916625441bd19df0280e34db9363004
CHARRIER Charlotte <charlotte.charrier97@gmail.com>
2024-10-17 16:38:08 +0200

    feat: ajout de spring security

---
+++ src/main/java/initiativedeuxsevres/ttm/service/UserSecurityService.java
@@ -0,0 +1,31 @@
+package initiativedeuxsevres.ttm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import initiativedeuxsevres.ttm.model.User;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserSecurityService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username)
+                .orElseThrow(()->new UsernameNotFoundException("User doesn't exist."));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+    }
+}
